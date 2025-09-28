b5cfdbcf9729841ed05e85c9158ab6ef4b101c3f
tiendatmagic <tiendatmagic8@gmail.com>
2025-09-28 13:11:58 +0700

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://apidev.miuxmmo.com/dangkhoashop/',
   apiUrlLocal: 'http://localhost:8000/',
 };
