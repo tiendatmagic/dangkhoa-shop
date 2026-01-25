df133bb98ca0b98896e79da919de33b93834e9ae
tiendatmagic <tiendatmagic8@gmail.com>
2026-01-25 12:12:19 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://apidev.miuxmmo.com/dangkhoashop/',
+  apiUrl: 'https://apidev.miummo.com/dangkhoashop/',
   apiUrlLocal: 'http://localhost:8000/',
 };
 
